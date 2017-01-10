cda316c79a453f5e69b3b5527e74e11c5f6b21cc
jonasyu <jonas.r.c.yu@oocl.com>
2017-01-10 22:49:05 +0800

    OkHttpUtil - add get and post method.

---
+++ app/src/main/java/com/oocl/com/teambuildmanagement/util/HttpUtil.java
@@ -0,0 +1,13 @@
+package com.oocl.com.teambuildmanagement.util;
+
+/**
+ * Created by YUJO2 on 1/10/2017.
+ */
+
+public class HttpUtil {
+    public static final String URL_IP = "http://112.74.166.187:8443";
+
+    public static final String URL_ACTIVITIES = "/api/activities";
+
+
+}
--- app/src/main/java/com/oocl/com/teambuildmanagement/util/OkHttpUtil.java
+++ app/src/main/java/com/oocl/com/teambuildmanagement/util/OkHttpUtil.java
@@ -1,8 +1,42 @@
 package com.oocl.com.teambuildmanagement.util;
 
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Author：Jonas Yu on 2017/1/2 01:43
  * Description:
  */
 public class OkHttpUtil {
+
+    public static final OkHttpClient mOkHttpClient = new OkHttpClient();
+
+    public static void get(String url, Callback callback){
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public static void post(String url, Map<String,String> params,Callback callback){
+        FormBody.Builder builder = new FormBody.Builder();
+        if(params != null && params.size() > 0){
+            for(Map.Entry<String,String> param: params.entrySet()){
+                builder.add(param.getKey(),param.getValue());
+            }
+        }
+        Request request = new Request.Builder()
+                .url(url)
+                .post(builder.build())
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(callback);
+    }
+
 }
